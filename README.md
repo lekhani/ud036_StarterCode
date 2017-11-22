1f6359728c2346186f9a7c2318b13e3c09c8d9bd
lekhani <lekhani@users.noreply.github.com>
2017-11-21 17:54:17 -0800

    updates per style guide
    Updated code per style guide and udacity forum mentor

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # ud036_StarterCode
 Source code for a Movie Trailer website.
 
-The code has been customized in media.py to open Google Chrome web browser instead of any default browser.
-The code to open the list of movies using a default browser is also included but commented out. 
-Movie trailer will play if the movie image is clicked.
+Run the code from Entertainment_center.py. It will create an html page with 
+6 movie names and images which we have chosen. The respective movie trailer 
+from youtube can be viewed if user clicks on the movie image.   
+The code has been customized in media.py to open Google Chrome web browser 
+instead of any default browser. There is also provision in media.py to use 
+system default browser instead of chrome browser in media.py file but the 
+code has been commented out. 
+
+The code also uses UDACITY provided module fresh_tomatoes.py to publish the 
+movie names, images and trailers.
+
+
--- media.py
+++ media.py
@@ -5,7 +5,8 @@ import time
 
 class Movie():
     """ This class saved information on movies"""
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image, 
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -13,12 +14,15 @@ class Movie():
         
         
     def show_trailer(self):
-# chrome_path is to explicitly use Google Chrome as web browser     
-         chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+        # chrome_path is to explicitly use Google Chrome as web browser     
+         chrome_path = "C:/Program Files (x86)/Google/Chrome/Application"
+         "/chrome.exe %s"
          webbrowser.get(chrome_path).open(self.trailer_youtube_url)
-#comment above two lines and uncomment below 1 line to use system default browser
-#        webbrowser.open(self.trailer_youtube_url)
-# time.sleep(<sec>) used for waiting certain seconds before executing next command       
-#         time.sleep(10)
-#         os.system("taskkill /im chrome.exe /f")
+         # To use system default browser 
+         # comment 3 lines of code above and uncomment 1 line below
+         # webbrowser.open(self.trailer_youtube_url)
+         # time.sleep(<sec>) used for waiting certain seconds before 
+         # executing the taskkill command for chrome       
+         # time.sleep(10)
+         # os.system("taskkill /im chrome.exe /f")
 
